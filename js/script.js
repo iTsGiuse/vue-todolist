a4474db3709b7ecb4180a4acd015bcaa83d11f7f
iTsGiuse <giuse.amato@icloud.com>
2024-04-09 18:45:36 +0200

    feat: add elimina task

--- js/script.js
+++ js/script.js
@@ -9,13 +9,16 @@ createApp({
     },
     methods: {
 
-        Aggiungi() {
+        aggiungi() {
             if (this.singolTask.trim() !== '') {
                 this.listTasks.unshift(this.singolTask);
                 this.singolTask= "";
             }
         }, 
 
+        elimina(index){
+            this.listTasks.splice(index, 1);
+        }
 
     },
 
